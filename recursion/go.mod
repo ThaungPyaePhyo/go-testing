564d84d41ebd9644e824ee1a628b244d8ddcbc23
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-07-11 00:30:05 +0700

    test

---
+++ recursion/go.mod
@@ -0,0 +1,3 @@
+module recursion.com
+
+go 1.22.4
---
+++ string/string.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+
+) 
+func main() {
+	const s = "helo"
+	fmt.Println(s,"length:",len(s))
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+	fmt.Println()
+
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+
+	for idx, runeValue := range s {
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
+
+	fmt.Println("\nUsing DecodeRuneInString")
+
+	for i, w := 0, 0; i < len(s); i += w {
+        runeValue, width := utf8.DecodeRuneInString(s[i:])
+        fmt.Printf("%#U starts at %d\n", runeValue, i)
+        w = width
+        examineRune(runeValue)
+    }
+}
+
+func examineRune(r rune) {
+    if r == 't' {
+        fmt.Println("found tee")
+    } else if r == 'ส' {
+        fmt.Println("found so sua")
+    }
+}
