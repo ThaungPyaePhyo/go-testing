63a34e26f7770135e92237238d8c651c891282de
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-08-06 23:55:29 +0700

    test

--- struce_embedding/sturce.go
+++ struce_embedding/sturce.go
@@ -2,8 +2,25 @@ package main
 
 import "fmt"
 
+func add(a int,b int) int {
+		return a + b
+	}
+
+func divide(a int, b int) (int,error) {
+	if b == 0 {
+		return 0, fmt.Errorf("cannot divide by zero")
+	}
+
+	return a / b, nil
+}
+
 func main() {
-	for i:= 0; i< 5; i++ {
-		fmt.Println(i);
-	}	
+	sum := add(3,4)
+	fmt.Println("sum:",sum)
+	quotient, err :=  divide(10,2)
+	if err != nil {
+		fmt.Println("Error:",err)
+	}else {
+		fmt.Println("Quotient:", quotient)
+	}
 }
