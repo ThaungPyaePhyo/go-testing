0b746f7b1a916d3c5492c0bf27ff9b3da56ceec6
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-08-06 23:59:34 +0700

    array

--- struce_embedding/sturce.go
+++ struce_embedding/sturce.go
@@ -2,25 +2,34 @@ package main
 
 import "fmt"
 
-func add(a int,b int) int {
-		return a + b
-	}
+// func add(a int,b int) int {
+// 		return a + b
+// 	}
 
-func divide(a int, b int) (int,error) {
-	if b == 0 {
-		return 0, fmt.Errorf("cannot divide by zero")
-	}
+// func divide(a int, b int) (int,error) {
+// 	if b == 0 {
+// 		return 0, fmt.Errorf("cannot divide by zero")
+// 	}
 
-	return a / b, nil
-}
+// 	return a / b, nil
+// }
 
+// func main() {
+// 	sum := add(3,4)
+// 	fmt.Println("sum:",sum)
+// 	quotient, err :=  divide(10,2)
+// 	if err != nil {
+// 		fmt.Println("Error:",err)
+// 	}else {
+// 		fmt.Println("Quotient:", quotient)
+// 	}
+// }
 func main() {
-	sum := add(3,4)
-	fmt.Println("sum:",sum)
-	quotient, err :=  divide(10,2)
-	if err != nil {
-		fmt.Println("Error:",err)
-	}else {
-		fmt.Println("Quotient:", quotient)
-	}
+	var arr [5]int
+	arr[0] = 1
+	arr[1] = 2
+	fmt.Println(arr)
+	 
+	numbers :=  [3] int{1,2,3}
+	fmt.Println(numbers)
 }
