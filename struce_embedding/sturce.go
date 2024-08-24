32417b2ee9651a18b64ab4ab89d62710c8e22a0b
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-08-24 14:12:04 +0700

    test net/http

--- struce_embedding/sturce.go
+++ struce_embedding/sturce.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 // func add(a int,b int) int {
 // 		return a + b
@@ -37,19 +40,84 @@ import "fmt"
 // }
 
 //slice
+// func main() {
+// 	var slice []int
+// 	slice = append(slice,1,2,3,4)
+// 	fmt.Println(slice)
 
+// 	a := []int{1,2,3,4,5}
+// 	fmt.Println(a)
+
+// 	s := make([]int,5)
+// 	s = append(1,2,3)
+// 	fmt.Println(s)
+
+// 	fruit := []string{"apple","orange","banana"}
+// 	fmt.Println(fruit)
+// }
+
+//Maps
+
+// func main() {
+// 	person := make(map[string]string)
+// 	person["name"] = "alice"
+// 	person["city"] = "new york"
+// 	fmt.Println(person)
+
+// 	countries := map[string]string{"Us": " united States", "CA": "Canada"}
+// 	fmt.Println(countries)
+// }
+
+//Struct
+
+// type Person struct {
+// 	name string
+// 	age int
+// }
+// func main() {
+// 	var p Person
+
+// 	p.name = "Alice"
+// 	p.age = 30
+// 	fmt.Println(p)
+
+// 	p2 := Person{name: "bo bo", age: 20}
+// 	fmt.Println(p2)
+// }
+
+// Methods
+// type Person struct {
+// 	name string
+// 	age int
+// }
+
+// func (p Person) greet() string{
+// 	return "Hello, my name is " + p.name
+// }
+
+// func main() {
+// 	p := Person{name: "koko", age:80}
+// 	fmt.Println(p.greet())
+// }
+
+// Pointer
+// func main() {
+// 	var x int = 10
+// 	var p *int = &x
+// 	fmt.Println("x:", x)
+// 	fmt.Println("p:", p)
+// 	fmt.Println("*p:", *p)
+
+// 	*p = 20
+//     fmt.Println("x after *p = 20:", x)
+
+// }
+
+func handler(w http.ResponseWriter,r *http.Request) {
+	fmt.Fprintf(w, "hello")
+}
 func main() {
-	var slice []int
-	slice = append(slice,1,2,3,4)
-	fmt.Println(slice)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8000", nil)
 
-	a := []int{1,2,3,4,5}
-	fmt.Println(a)
-
-	s := make([]int,5)
-	s = append(1,2,3)
-	fmt.Println(s)
-
-	fruit := []string{"apple","orange","banana"}
-	fmt.Println(fruit)
 }
